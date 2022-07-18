96ba4181476d36858a766ac961170bc539159d1c
Marvin Virapin <marvin.virapin@hotmail.fr>
2022-07-18 09:53:41 +0200

    premier commit

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alibobo</title>
+    <link href="./assets/css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php?page=accueil">Accueil</a></li>
+                <li><a href="index.php?page=articles">Articles</a></li>
+                <li><a href="index.php?page=inscription">Inscription</a></li>
+                <li><a href="index.php?page=login">Login</a></li>
+            </ul>
+        </nav>
+    </header>
